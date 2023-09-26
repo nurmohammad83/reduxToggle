b82e63836bcee31745dd70b858faf47532892525
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-26 14:56:05 +0600

    fix: format  banner code

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -1,35 +1,45 @@
-import { RootState } from "../store"
-import { useSelector} from 'react-redux'
+import { RootState } from "../store";
+import { useSelector } from "react-redux";
 
 const Banner = () => {
-    const dark = useSelector((state: RootState) => state.toggle.value)
+  const dark = useSelector((state: RootState) => state.toggle.value);
   return (
     <section className={`bg-gray-50 dark:bg-black`}>
-  <div
-    className="mx-auto max-w-screen-xl px-4 py-24 lg:flex lg:h-screen lg:items-center"
-  >
-    <div className="mx-auto max-w-xl text-center">
-      <h1 className= {`${dark?'bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl text-transparent sm:text-5xl':'text-3xl  sm:text-5xl'} font-extrabold`}>
-        Understand User Flow.
-        <strong className={`${dark?'bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl text-transparent sm:text-5xl' : 'text-red-700'} font-extrabold sm:block`}>
-          Increase Conversion.
-        </strong>
-      </h1>
+      <div className="mx-auto max-w-screen-xl px-4 py-24 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-xl text-center">
+          <h1
+            className={`${
+              dark
+                ? "bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl text-transparent sm:text-5xl"
+                : "text-3xl  sm:text-5xl"
+            } font-extrabold`}
+          >
+            Understand User Flow.
+            <strong
+              className={`${
+                dark
+                  ? "bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl text-transparent sm:text-5xl"
+                  : "text-red-700"
+              } font-extrabold sm:block`}
+            >
+              Increase Conversion.
+            </strong>
+          </h1>
 
-      <p className={`text-black dark:text-white mt-4 sm:text-xl/relaxed`}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-        tenetur fuga ducimus numquam ea!
-      </p>
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <button
-          className={`dark:bg-white dark:text-black bg-red-600 hover:bg-red-700 text-white block w-full rounded  px-12 py-3 text-sm font-medium shadow  sm:w-auto`}
-        >
-          Get Started
-        </button>
+          <p className={`text-black dark:text-white mt-4 sm:text-xl/relaxed`}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            illo tenetur fuga ducimus numquam ea!
+          </p>
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <button
+              className={`dark:bg-white dark:text-black bg-red-600 hover:bg-red-700 text-white block w-full rounded  px-12 py-3 text-sm font-medium shadow  sm:w-auto`}
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
-  )
-}
-export default Banner
+    </section>
+  );
+};
+export default Banner;
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,32 +1,39 @@
-import { RootState } from "../store"
-import { useSelector, useDispatch } from 'react-redux'
-import { themToggled } from "../store/slices/toggleSlice"
-import { useEffect } from "react"
+import { RootState } from "../store";
+import { useSelector, useDispatch } from "react-redux";
+import { themToggled } from "../store/slices/toggleSlice";
+import { useEffect } from "react";
 const Header = () => {
-    const dark = useSelector((state: RootState) => state.toggle.value)
-    const dispatch = useDispatch()
+  const dark = useSelector((state: RootState) => state.toggle.value);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      if (dark) {
-        document.documentElement.setAttribute("class", "dark");
-        localStorage.setItem("dark", "true");
-      } else {
-        document.documentElement.removeAttribute("class");
-        localStorage.setItem("dark", "false");
-      }
-    }, [dark]);
+  useEffect(() => {
+    if (dark) {
+      document.documentElement.setAttribute("class", "dark");
+      localStorage.setItem("dark", "true");
+    } else {
+      document.documentElement.removeAttribute("class");
+      localStorage.setItem("dark", "false");
+    }
+  }, [dark]);
   return (
-    <header className={`bg-gray-50 dark:bg-gray-800 h-16 p-5 flex justify-between items-center px-12  border-b`}>
-       
-            <h1 className={`text-black dark:text-white text-lg font-bold `}>Redux Toggle</h1>
-            <div  >
-            <label className="relative inline-flex items-center cursor-pointer">
-      <input onClick={()=>dispatch(themToggled())} type="checkbox" value="" className="sr-only peer" />
-      <div className="w-11 h-6  bg-gray-300  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-    </label>
-           
-        </div>
+    <header
+      className={`bg-gray-50 dark:bg-gray-800 h-16 p-5 flex justify-between items-center px-12  border-b`}
+    >
+      <h1 className={`text-black dark:text-white text-lg font-bold `}>
+        Redux Toggle
+      </h1>
+      <div>
+        <label className="relative inline-flex items-center cursor-pointer">
+          <input
+            onClick={() => dispatch(themToggled())}
+            type="checkbox"
+            value=""
+            className="sr-only peer"
+          />
+          <div className="w-11 h-6  bg-gray-300  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        </label>
+      </div>
     </header>
-  )
-}
-export default Header
+  );
+};
+export default Header;
